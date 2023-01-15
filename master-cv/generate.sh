4a8481a0004ed686eeaf33b4f685cde92e63e6c5
Matthew Burke <matthew@rijalburke.com>
2023-01-15 12:26:54 +0000

    Init CV pipeline

---
+++ master-cv/generate.sh
@@ -0,0 +1,15 @@
+# sudo apt install cmark wkhtmltopdf
+
+RUN_DIR=$PWD
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+cd $SCRIPT_DIR
+
+rm matthew-burke.html
+cat pre.html >> matthew-burke.html
+cmark matthew-burke.md >> matthew-burke.html
+cat post.html >> matthew-burke.html
+
+wkhtmltopdf matthew-burke.html matthew-burke.pdf
+
+cd $RUN_DIR
---
+++ master-cv/matthew-burke.md
@@ -0,0 +1,8 @@
+# Matthew Burke
+
+[matthew.rijalburke.com](https://matthew.rijalburke.com)  
+[matthew@rijalburke.com](mailto:matthew@rijalburke.com)
+
+## Education
+
+## Work experience
